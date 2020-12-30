9773d2dc5b4c7df22d0b43b08e721bdce6aab899
Victor <vlouisa@qualogy.com>
2020-12-30 11:08:40 +0100

    add acceptance test for employee

---
+++ src/main/java/nl/louisa/booking/company/service/PolicyService.java
@@ -0,0 +1,9 @@
+package nl.louisa.booking.company.service;
+
+import nl.louisa.booking.hotel.domain.RoomType;
+
+public class PolicyService {
+    public void setEmployeePolicy(String employeeId, RoomType ... roomTypesAllowed) {
+        throw new UnsupportedOperationException();
+    }
+}
--- src/test/java/nl/louisa/booking/features/EmployeeFeaturesTest.java
+++ src/test/java/nl/louisa/booking/features/EmployeeFeaturesTest.java
@@ -1,14 +1,15 @@
 package nl.louisa.booking.features;
 
 import nl.louisa.booking.company.domain.Employee;
+import nl.louisa.booking.company.service.CompanyService;
+import nl.louisa.booking.company.service.PolicyService;
 import nl.louisa.booking.employee.domain.BookingCheck;
 import nl.louisa.booking.employee.domain.DateCheck;
+import nl.louisa.booking.employee.domain.RoomTypeCheck;
 import nl.louisa.booking.employee.service.BookingChecks;
 import nl.louisa.booking.employee.service.BookingService;
-import nl.louisa.booking.company.service.CompanyService;
-import nl.louisa.booking.employee.domain.RoomTypeCheck;
-import nl.louisa.booking.hotel.service.HotelService;
 import nl.louisa.booking.hotel.domain.Hotel;
+import nl.louisa.booking.hotel.service.HotelService;
 import nl.louisa.booking.shared.repository.Repository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,7 @@ import java.time.LocalDate;
 
 import static nl.louisa.booking.hotel.domain.RoomType.DOUBLE;
 import static nl.louisa.booking.hotel.domain.RoomType.EXECUTIVE;
+import static nl.louisa.booking.hotel.domain.RoomType.SINGLE;
 import static nl.louisa.booking.hotel.domain.RoomType.SUITE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -24,6 +26,8 @@ public class EmployeeFeaturesTest {
 
     private static final LocalDate JUNE_1ST = LocalDate.of(2020, 6, 1);
     private static final LocalDate JUNE_6TH = LocalDate.of(2020, 6, 6);
+
+    private PolicyService policyService;
     private CompanyService companyService;
     private HotelService hotelService;
     private BookingService bookingService;
@@ -37,6 +41,7 @@ public class EmployeeFeaturesTest {
         final BookingCheck roomTypeCheck = new RoomTypeCheck(hotelRepository);
         final BookingChecks bookingChecks = new BookingChecks(dateCheck, roomTypeCheck);
 
+        policyService = new PolicyService();
         companyService = new CompanyService(employeeRepository);
         hotelService = new HotelService(hotelRepository);
         bookingService = new BookingService(bookingChecks);
@@ -65,5 +70,19 @@ public class EmployeeFeaturesTest {
         assertThatThrownBy(() -> bookingService.book("BW", "WAS", DOUBLE, JUNE_6TH, JUNE_1ST))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("Booking cancelled: Check-in should be before check-out");
+    }
+
+    @Test
+    void booking_a_hotel_when_employee_policy_does_not_allow_booking() {
+        hotelService.addHotel("WAS", "Waldorf Astoria");
+        hotelService.setRoom("WAS", 10 , SINGLE);
+        hotelService.setRoom("WAS", 5 , DOUBLE);
+
+        companyService.addEmployee("WEN", "BW");
+        policyService.setEmployeePolicy("BW", SINGLE, DOUBLE);
+
+        assertThatThrownBy(() -> bookingService.book("BW", "WAS", EXECUTIVE, JUNE_1ST, JUNE_6TH))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Booking cancelled: Room can't be booked because of policy");
     }
 }
